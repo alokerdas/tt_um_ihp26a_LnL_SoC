1b1abfa50c016641745b5cfb98d0ef3ce5f06824
alokerdas <aloke.das@ieee.org>
2026-02-06 16:01:50 +0530

    Removed the loopback test

--- test/test.py
+++ test/test.py
@@ -5,30 +5,6 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
-
-@cocotb.test()
-async def test_loopback(dut):
-    dut._log.info("Start")
-
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())
-
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    assert dut.uio_oe.value == 255
-
-    # When under reset: Output is uio_in, uio is in input mode
-    for i in range(256):
-        dut.ui_in.value = i
-        await ClockCycles(dut.clk, 1)
-        assert dut.uo_out.value == i
-
 @cocotb.test()
 async def test_cpu (dut):
     dut._log.info("Start")
