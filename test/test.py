a4d8486924dcbd0cf3835329a185423b698d54a0
Aloke Das <aloke.das@ieee.org>
2026-02-12 20:14:50 +0530

    Added CPU tests

--- test/test.py
+++ test/test.py
@@ -16,6 +16,7 @@ async def test_cpu (dut):
     dut.intrin.value = 0
     dut._log.info("Testing reset")
     dut.rst_n.value = 0
+    dut.intrin.value = 0
     await ClockCycles(dut.clk, 3)
     assert dut.ewire.value == 0
     assert dut.twire.value == 1
@@ -30,8 +31,142 @@ async def test_cpu (dut):
     await ClockCycles(dut.clk, 1)
     assert dut.uio_oe.value == 252
     dut._log.info("Testing CPU and BOOT ROM")
+    dut._log.info("Testing Initial")
     await ClockCycles(dut.clk, 1)
-    dut._log.info("Testing SKI")
     assert dut.irwire.value == 0
-    # dut.intrin.value = 1
-    # dut.ui_in.value = 119
+    dut._log.info("Testing SKI")
+    await ClockCycles(dut.clk, 3)
+    assert dut.irwire.value == 61952
+    dut._log.info("Testing SKI and BUN")
+    await ClockCycles(dut.clk, 5)
+    assert dut.irwire.value == 16384
+    dut._log.info("Testing BUN")
+    dut.intrin.value = 1 # This is needed for next SKI
+    await ClockCycles(dut.clk, 8)
+    assert dut.irwire.value == 61952
+    dut._log.info("Testing SKI")
+    dut.ui_in.value = 119 # This is needed for next INP
+    await ClockCycles(dut.clk, 5)
+    assert dut.irwire.value == 63488
+    dut._log.info("Testing INP")
+    await ClockCycles(dut.clk, 1)
+    assert dut.acwire.value == 119
+    dut._log.info("Testing ADD")
+    await ClockCycles(dut.clk, 4)
+    assert dut.irwire.value == 4103
+    await ClockCycles(dut.clk, 3)
+    assert dut.drwire.value == 17
+    await ClockCycles(dut.clk, 1)
+    assert dut.acwire.value == 136
+    dut._log.info("Testing OUT")
+    await ClockCycles(dut.clk, 4)
+    assert dut.irwire.value == 62464
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 136
+    dut._log.info("Testing SPA")
+    await ClockCycles(dut.clk, 10)
+    assert dut.irwire.value == 28688
+    dut._log.info("Testing LDA")
+    await ClockCycles(dut.clk, 5)
+    assert dut.irwire.value == 8199
+    await ClockCycles(dut.clk, 4)
+    assert dut.acwire.value == 17
+    dut._log.info("Testing AND")
+    await ClockCycles(dut.clk, 4)
+    assert dut.irwire.value == 11
+    await ClockCycles(dut.clk, 3)
+    assert dut.drwire.value == 128
+    await ClockCycles(dut.clk, 1)
+    assert dut.acwire.value == 0
+    dut._log.info("Testing SZA")
+    await ClockCycles(dut.clk, 4)
+    assert dut.irwire.value == 28676
+    dut._log.info("Testing LDAI")
+    await ClockCycles(dut.clk, 5)
+    assert dut.irwire.value == 40967
+    await ClockCycles(dut.clk, 3)
+    assert dut.drwire.value == 17
+    await ClockCycles(dut.clk, 3)
+    assert dut.acwire.value == 7
+    dut._log.info("Testing ADDI")
+    await ClockCycles(dut.clk, 4)
+    assert dut.irwire.value == 36881
+    await ClockCycles(dut.clk, 6)
+    assert dut.acwire.value == 24
+    dut._log.info("Testing CMA")
+    await ClockCycles(dut.clk, 12)
+    assert dut.irwire.value == 29184
+    await ClockCycles(dut.clk, 1)
+    assert dut.acwire.value == 65511
+    dut._log.info("Testing SNA")
+    await ClockCycles(dut.clk, 4)
+    assert dut.irwire.value == 28680
+    dut._log.info("Testing CIR")
+    await ClockCycles(dut.clk, 5)
+    assert dut.irwire.value == 28800
+    await ClockCycles(dut.clk, 1)
+    assert dut.acwire.value == 32755
+    dut._log.info("Testing OUT")
+    await ClockCycles(dut.clk, 5)
+    assert dut.irwire.value == 62464
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 243
+    dut._log.info("Testing CLE")
+    assert dut.ewire.value == 1 # this is because of earlier CIR
+    await ClockCycles(dut.clk, 4)
+    assert dut.irwire.value == 29696
+    await ClockCycles(dut.clk, 1)
+    assert dut.ewire.value == 0
+    dut._log.info("Testing CIL")
+    await ClockCycles(dut.clk, 4)
+    assert dut.irwire.value == 28736
+    await ClockCycles(dut.clk, 1)
+    assert dut.acwire.value == 65510
+    dut._log.info("Testing OUT")
+    await ClockCycles(dut.clk, 5)
+    assert dut.irwire.value == 62464
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 230
+    dut._log.info("Testing INC")
+    await ClockCycles(dut.clk, 4)
+    assert dut.irwire.value == 28704
+    await ClockCycles(dut.clk, 1)
+    assert dut.acwire.value == 65511
+    dut._log.info("Testing CME")
+    assert dut.ewire.value == 0
+    await ClockCycles(dut.clk, 4)
+    assert dut.irwire.value == 28928
+    await ClockCycles(dut.clk, 1)
+    assert dut.ewire.value == 1
+    dut._log.info("Testing CIL")
+    await ClockCycles(dut.clk, 4)
+    assert dut.irwire.value == 28736
+    await ClockCycles(dut.clk, 1)
+    assert dut.acwire.value == 65487
+    dut._log.info("Testing OUT")
+    await ClockCycles(dut.clk, 5)
+    assert dut.irwire.value == 62464
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 207
+    dut._log.info("Testing BSA")
+    await ClockCycles(dut.clk, 4)
+    assert dut.irwire.value == 20510
+    await ClockCycles(dut.clk, 1)
+    assert dut.arwire.value == 30
+    await ClockCycles(dut.clk, 5)
+    assert dut.arwire.value == 31
+    dut._log.info("Testing BUNI")
+    await ClockCycles(dut.clk, 2)
+    assert dut.irwire.value == 49182
+    await ClockCycles(dut.clk, 1)
+    assert dut.arwire.value == 30
+    await ClockCycles(dut.clk, 2)
+    assert dut.arwire.value == 28
+    dut._log.info("Testing ISZ")
+    await ClockCycles(dut.clk, 5)
+    assert dut.irwire.value == 24705
+    await ClockCycles(dut.clk, 1)
+    assert dut.arwire.value == 129
+    dut._log.info("Testing HLT")
+    await ClockCycles(dut.clk, 10)
+    assert dut.irwire.value == 28673
