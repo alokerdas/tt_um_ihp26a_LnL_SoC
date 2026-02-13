a53e80186df0325a7614dae11357173d9fb464fa
alokerdas <aloke.das@ieee.org>
2026-02-13 11:17:57 +0530

    Reverted the changes for CPU test

--- src/bootrom.v
+++ src/bootrom.v
@@ -62,7 +62,7 @@ module bootrom (
   end
   always @ (posedge romclk or posedge rst) begin
     if (rst) begin
-      outbuf5 <= 16'h3080; // STA 80 -> spi
+      outbuf5 <= 16'h3050; // STA 50 -> spi
     end else begin
       outbuf5 <= 16'h0000;
     end
@@ -104,7 +104,7 @@ module bootrom (
   end
   always @ (posedge romclk or posedge rst) begin
     if (rst) begin
-      outbufB <= 16'h0080; // data
+      outbufB <= 16'h0050; // data
     end else begin
       outbufB <= 16'h0000;
     end
@@ -224,7 +224,7 @@ module bootrom (
   end
   always @ (posedge romclk or posedge rst) begin
     if (rst) begin
-      outbuf1C <= 16'h6081; // ISZ 51 icrement register inside spi
+      outbuf1C <= 16'h6051; // ISZ 51 icrement register inside spi
     end else begin
       outbuf1C <= 16'h0000;
     end
